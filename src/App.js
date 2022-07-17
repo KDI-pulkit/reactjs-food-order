972a3da600a2017c20d21811ced51e8d9f4214a6
Pulkit Mittal <pulkit.mittal@kongsbergdigital.com>
2022-07-17 14:48:16 +0530

    added cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react'; 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartISShown] = useState(false);
+
+  function showCartHandler(){
+    setCartISShown(true);
+  }
+
+  function hideCartHandler(){
+    setCartISShown(false);
+  }
+
+
   return (
     <CartProvider>
-      <Header />
+      { cartIsShown && <div> Cart... </div>}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
